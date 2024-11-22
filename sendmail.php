334fcfa6d8836b26d48ad97f8e53d0be3108bd94
tkulagina <tkulagina1974@gmail.com>
2024-11-22 18:00:46 +0200

    add php

---
+++ sendmail.php
@@ -0,0 +1,73 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('uk', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+	//От кого письмо
+	$mail->setFrom('tkulagina1974@gmail.com', 'regulatorMIR');
+	//Кому отправить
+	$mail->addAddress('regulatormir@gmail.com');
+	//Тема письма
+	$mail->Subject = 'Нове замовлення!';
+/*
+	//Рука
+	$hand = "Правая";
+	if($_POST['hand'] == "left"){
+		$hand = "Левая";
+	}
+ */ 
+	//Тело письма
+	$body = '<h1>Нове замовлення!</h1>';
+	
+	if(trim(!empty($_POST['name']))){
+		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+	}
+	
+	if(trim(!empty($_POST['phone']))){
+		$body.='<p><strong>Телефон:</strong> '$_POST['phone'].'</p>';
+	}
+    if(trim(!empty($_POST['email']))){
+		$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+	}
+    /* 
+	if(trim(!empty($_POST['age']))){
+		$body.='<p><strong>Возраст:</strong> '.$_POST['age'].'</p>';
+	}*/
+	
+	if(trim(!empty($_POST['message']))){
+		$body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
+	}
+	/* 
+	//Прикрепить файл
+	if (!empty($_FILES['image']['tmp_name'])) {
+		//путь загрузки файла
+		$filePath = __DIR__ . "/files/" . $_FILES['image']['name']; 
+		//грузим файл
+		if (copy($_FILES['image']['tmp_name'], $filePath)){
+			$fileAttach = $filePath;
+			$body.='<p><strong>Фото в приложении</strong>';
+			$mail->addAttachment($fileAttach);
+		}
+	}*/
+
+	$mail->Body = $body;
+
+	//Отправляем
+	if (!$mail->send()) {
+		$message = 'Помилка';
+	} else {
+		$message = 'Дані надіслані!';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
