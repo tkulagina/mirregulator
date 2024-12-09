96d15639c041cfe52adf0176f1e3187a338a2530
tkulagina <tkulagina1974@gmail.com>
2024-12-09 20:10:33 +0200

    update

--- sendmail.php
+++ sendmail.php
@@ -11,7 +11,7 @@
 	$mail->IsHTML(true);
 
 	//От кого письмо
-	$mail->setFrom('tkulagina1974@gmail.com', 'regulatorMIR');
+	$mail->setFrom('main@regulatormir.com', 'regulatorMIR');
 	//Кому отправить
 	$mail->addAddress('regulatormir@gmail.com');
 	//Тема письма
@@ -29,10 +29,10 @@
 	if(trim(!empty($_POST['name']))){
 		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
 	}
-	
+	/*
 	if(trim(!empty($_POST['phone']))){
 		$body.='<p><strong>Телефон:</strong> '$_POST['phone'].'</p>';
-	}
+	}*/
     if(trim(!empty($_POST['email']))){
 		$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
 	}
