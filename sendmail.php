9c47400182e35209b2872bca24f3693e1f3de6d4
tkulagina <tkulagina1974@gmail.com>
2024-12-11 11:27:10 +0200

    update php

--- sendmail.php
+++ sendmail.php
@@ -11,51 +11,28 @@
 	$mail->IsHTML(true);
 
 	//От кого письмо
-	$mail->setFrom('main@regulatormir.com', 'regulatorMIR');
+	$mail->setFrom('main@regulatormir.com');
 	//Кому отправить
 	$mail->addAddress('regulatormir@gmail.com');
 	//Тема письма
 	$mail->Subject = 'Нове замовлення!';
-/*
-	//Рука
-	$hand = "Правая";
-	if($_POST['hand'] == "left"){
-		$hand = "Левая";
-	}
- */ 
+
 	//Тело письма
 	$body = '<h1>Нове замовлення!</h1>';
 	
 	if(trim(!empty($_POST['name']))){
 		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
 	}
-	/*
-	if(trim(!empty($_POST['phone']))){
-		$body.='<p><strong>Телефон:</strong> '$_POST['phone'].'</p>';
-	}*/
+	
     if(trim(!empty($_POST['email']))){
 		$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
 	}
-    /* 
-	if(trim(!empty($_POST['age']))){
-		$body.='<p><strong>Возраст:</strong> '.$_POST['age'].'</p>';
-	}*/
+   
 	
 	if(trim(!empty($_POST['message']))){
 		$body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
 	}
-	/* 
-	//Прикрепить файл
-	if (!empty($_FILES['image']['tmp_name'])) {
-		//путь загрузки файла
-		$filePath = __DIR__ . "/files/" . $_FILES['image']['name']; 
-		//грузим файл
-		if (copy($_FILES['image']['tmp_name'], $filePath)){
-			$fileAttach = $filePath;
-			$body.='<p><strong>Фото в приложении</strong>';
-			$mail->addAttachment($fileAttach);
-		}
-	}*/
+	
 
 	$mail->Body = $body;
 
